65f3994828d9fe76c4919eda3a9102721f2cd109
Thiago Gerbi <gerbithiago@gmail.com>
2025-05-23 18:59:15 -0300

    Conectando com o frontend e com o database

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const testeRoute = require('./src/routes/testeRoute');
+const cors = require('cors')
 
 dotenv.config();
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-
-const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.use('/api', testeRoute);
+app.use('/', testeRoute);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta: ${PORT}`);
